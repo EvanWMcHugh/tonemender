859d80cd529fcc835b4e5971e1549c279704a9c1
Evan McHugh <evan.w.mchugh@gmail.com>
2026-02-05 13:36:45 -0800

    Add native iOS app scaffold

---
+++ ios/ToneMender/ToneMender/ToneMenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneMenderApp.swift
+//  ToneMender
+//
+//  Created by Evan McHugh on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ToneMenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
