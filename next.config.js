3e6ba697b6e4e744aceec49ceb877cf722ccb150
w8r442crmh-debug <w8r442crmh@privaterelay.appleid.com>
2026-05-03 16:39:40 -0700

    done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      "/*": ["./certs/**/*"],
-    },
-  },
 };
 
 module.exports = nextConfig;
