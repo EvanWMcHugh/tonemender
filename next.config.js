26099d883a900e39cb2146a50fcc05168918cb00
w8r442crmh-debug <w8r442crmh@privaterelay.appleid.com>
2026-05-03 16:16:19 -0700

    Fix Apple billing cert bundling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/billing/apple/sync": ["./certs/**/*"],
+    },
+  },
 };
 
 module.exports = nextConfig;
