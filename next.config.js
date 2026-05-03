530c7f15924a75d7bba95feb67ad35ae142ea1c3
w8r442crmh-debug <w8r442crmh@privaterelay.appleid.com>
2026-05-03 16:21:32 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputFileTracingIncludes: {
-      "/api/billing/apple/sync": ["./certs/**/*"],
+      "/*": ["./certs/**/*"],
     },
   },
 };
